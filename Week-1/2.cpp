ee3b907ba3f0163e981a18205fb95e00c00079f6
AKSHAY <forakshay.dev@gmail.com>
2023-02-03 20:22:56 +0530

    1

---
+++ Week-1/2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+	short i=2300, j=4322;
+	cout<<i+j;
+}
---
+++ Week-1/3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int l;
+    int b;
+    cin>>l;
+    cin>>b;
+	int P = 2*(l+b);
+	cout<<"Perimeter="<< P;
+}
