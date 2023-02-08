b7921927e9e789a1e107e017ba472cd25c63d395
AKSHAY <forakshay.dev@gmail.com>
2023-02-09 01:24:21 +0530

    7 updated

--- Week-1/7 working.cpp
+++ Week-1/7 working.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
 using namespace std;
-int main() {
-  int n;
-  std::cin>>n;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n - i; j++) {
-      printf("  ");
+int main(){
+    int n;
+    cin>>n;
+    for (int i=0; i<n; ++i) {
+        int k=0;
+        for (int j=0; j<n*2-1; ++j) {
+            if(j<n-i-1){
+                cout<<" ";
+            }
+            else if(k < 2*i+1){
+                if(k==0 || k == 2*i || i == n - 1){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+                k++;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
     }
-    for (int j = i; j < 2 * i; j++) {
-      printf("%d ", j);
-    }
-    int ele = 2 * (i - 1);
-    for (int j = 1; j <= i - 1; j++) {
-      printf("%d ", ele--);
-    }
-    printf("\n");
-  }
-  return 0;
 }
