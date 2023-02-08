50063c578986f4b0effd720837098febfd4bf43a
AKSHAY <forakshay.dev@gmail.com>
2023-02-09 01:21:57 +0530

    updated

--- Week-1/7 working.cpp
+++ Week-1/7 working.cpp
@@ -4,13 +4,13 @@ int main() {
   int n;
   std::cin>>n;
   for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n - i - 1; j++) {
+    for (int j = 1; j <= n - i; j++) {
       printf("  ");
     }
-    for (int j = i; j < i; j++) {
+    for (int j = i; j < 2 * i; j++) {
       printf("%d ", j);
     }
-    int ele = 2 * (n - i - 1);
+    int ele = 2 * (i - 1);
     for (int j = 1; j <= i - 1; j++) {
       printf("%d ", ele--);
     }
